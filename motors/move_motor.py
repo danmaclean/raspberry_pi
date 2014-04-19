677544642e690eaad81474cf9bd9513d69ab97c6
Dan MacLean <maclean.daniel@gmail.com>
2014-04-19 20:14:10 +0100

    updated code in move_motors.py

--- motors/move_motor.py
+++ motors/move_motor.py
@@ -1,49 +1,58 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-untitled.py
+move_motor.py
 
-Created by Dan MacLean (TSL) on 2014-04-19.
+Created by Dan MacLean on 2014-04-19.
 Copyright (c) 2014 Dan MacLean. All rights reserved.
 """
 
 import sys
 import getopt
-import RPi.GPIO as GPIO, time
+import RPi.GPIO as io
 
 help_message = '''
-The help message goes here.
+simple script for running a 6v DC motor through the Pi using the adfruit occi-
+dentalis kernal module for PWM.
 '''
 
+io.setmode(io.BCM)
 
-class Usage(Exception):
-	def __init__(self, msg):
-		self.msg = msg
+in1_pin = 4
+in2_pin = 17
 
+io.setup(in1_pin, io.OUT)
+io.setup(in2_pin, io.OUT)
 
-def main(argv=None):
-	if argv is None:
-		argv = sys.argv
-	try:
-		try:
-			opts, args = getopt.getopt(argv[1:], "ho:v", ["help", "output="])
-		except getopt.error, msg:
-			raise Usage(msg)
-	
-		# option processing
-		for option, value in opts:
-			if option == "-v":
-				verbose = True
-			if option in ("-h", "--help"):
-				raise Usage(help_message)
-			if option in ("-o", "--output"):
-				output = value
-	
-	except Usage, err:
-		print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
-		print >> sys.stderr, "\t for help use --help"
-		return 2
+def set(property, value):
+    try:
+        f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
+        f.write(value)
+        f.close()	
+    except:
+        print("Error writing to: " + property + " value: " + value)
+ 
+set("delayed", "0")
+set("mode", "pwm")
+set("frequency", "500")
+set("active", "1")
 
+def clockwise():
+    io.output(in1_pin, True)    
+    io.output(in2_pin, False)
 
-if __name__ == "__main__":
-	sys.exit(main())
+def counter_clockwise():
+    io.output(in1_pin, False)
+    io.output(in2_pin, True)
+
+clockwise()
+
+while True:
+    cmd = raw_input("Command, f/r 0..9, E.g. f5 :")
+    direction = cmd[0]
+    if direction == "f":
+        clockwise()
+    else: 
+        counter_clockwise()
+    speed = int(cmd[1]) * 11
+    set("duty", str(speed))
