2b34b993fdf0e865cbb91c6d66805f876d24032b
danmaclean <maclean.daniel@gmail.com>
2015-04-08 10:38:51 +0100

    added non adafruit script

---
+++ magic_clock/untitled.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+beggining ... 
+
+Created by Dan MacLean on 2014-04-19.
+Copyright (c) 2014 Dan MacLean. All rights reserved.
+"""
+
+import sys
+import os
+import unittest
+
+
+class untitled:
+	def __init__(self):
+		pass
+
+
+class untitledTests(unittest.TestCase):
+	def setUp(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
