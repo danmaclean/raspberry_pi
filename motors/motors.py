2c7d08e42b8394e44701e8ff886db1711040d6f1
danmaclean <maclean.daniel@gmail.com>
2015-04-08 11:13:55 +0100

    added non adafruit script

--- motors/motors.py
+++ motors/motors.py
@@ -2,23 +2,32 @@ import RPi.GPIO as GPIO
 from time import sleep
  
 GPIO.setmode(GPIO.BCM)
+
+def setup():
+    '''Sets up the pins'''
+    Motor1A = 4 #pin A
+    Motor1B = 17 #pin B
+    Motor1E = 18 #pin Enable
  
-Motor1A = 4
-Motor1B = 17
-Motor1E = 18
- 
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1E,GPIO.OUT)
- 
-print "Turning motor on"
-GPIO.output(Motor1A,GPIO.HIGH)
-GPIO.output(Motor1B,GPIO.LOW)
-GPIO.output(Motor1E,GPIO.HIGH)
- 
+    #tell pi what these pins are and whether they are in or output - in this case output... 
+    GPIO.setup(Motor1A,GPIO.OUT)
+    GPIO.setup(Motor1B,GPIO.OUT)
+    GPIO.setup(Motor1E,GPIO.OUT)
+
+def go_clockwise(): 
+    print "Going clockwise"
+    GPIO.output(Motor1A,GPIO.HIGH)
+    GPIO.output(Motor1B,GPIO.LOW)
+    GPIO.output(Motor1E,GPIO.HIGH)
+
+def stop():
+     print "Stopping motor"
+     GPIO.output(Motor1E,GPIO.LOW)
+     
+setup()
+go_clockwise() 
 sleep(2)
+stop()
  
-print "Stopping motor"
-GPIO.output(Motor1E,GPIO.LOW)
  
 GPIO.cleanup()
