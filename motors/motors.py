365504d92509d7edd1fe2910258ef5dfe3134ad9
danmaclean <maclean.daniel@gmail.com>
2015-04-08 11:59:02 +0100

    added non adafruit script

--- motors/motors.py
+++ motors/motors.py
@@ -16,12 +16,12 @@ class Motor:
         GPIO.setup(self.a, GPIO.OUT)
         GPIO.setup(self.b, GPIO.OUT)
         GPIO.setup(self.e, GPIO.OUT)
-
+    
     def degrees_to_time(degrees):
         time = degrees * Motor.SECONDS_PER_DEGREE
         print "time to run= ", time
         return time
-        
+    
     def go_clockwise(self,degrees=90):
         print degrees, " degrees to do"
         GPIO.output(self.a,GPIO.HIGH)
@@ -29,14 +29,14 @@ class Motor:
         GPIO.output(self.e,GPIO.HIGH)
         sleep(self.degrees_to_time(degrees))
         self.stop()
-
+    
     def go_anticlockwise(self,degrees=90):
         GPIO.output(self.a,GPIO.LOW)
         GPIO.output(self.b,GPIO.HIGH)
         GPIO.output(self.e,GPIO.HIGH)
         sleep(self.degrees_to_time(degrees))
         self.stop()        
-
+    
     def stop(self):
         GPIO.output(self.e,GPIO.LOW)
     
