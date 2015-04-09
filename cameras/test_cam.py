6c04db546c25d0bab09d05888d489f259b55743d
danmaclean <maclean.daniel@gmail.com>
2015-04-09 14:53:55 +0100

    test IR

---
+++ cameras/test_cam.py
@@ -0,0 +1,24 @@
+import IRCamera as ir
+
+def do_plot(original_image, scaled_image):
+    fig = plt.figure(figsize=(10,3))
+    
+    axes1 = fig.add_subplot(1,3,1)
+    axes2 = fig.add_subplot(1,3,2)
+    axes3 = fig.add_subplot(1,3,3)
+    
+    axes1.set_ylabel("Raw")
+    axes2.set_ylabel('Grey')
+    axes3.set_ylabel('GreyReds')
+    
+    axes1.imshow(original_image)
+    axes2.imshow(scaled_image,cmap='GreyIntensity')
+    axes3.imshow(scaled_image,cmap='RedSplit')
+    
+    fig.tight_layout()
+    plt.show(fig)
+
+
+image = ir.get_rgb_array(width,height)
+scaled_image = ir.enhance_ir(image)
+do_plot(image,scaled_image)
