7cf67fc9306f4622370db9c4f4e5c7e97948db62
danmaclean <maclean.daniel@gmail.com>
2015-04-14 10:34:40 +0100

    checking splits

---
+++ cameras/do_camera.py
@@ -0,0 +1,21 @@
+
+import picamera import os
+
+ prefix = 'ir_image'
+suffix = '.jpg'
+number = 0
+filename = prefix + str(number) + suffix
+
+files = os.listdir('.')
+
+while filename in files
+    number += 1
+    filename = prefix + str(number) + suffix
+
+cam = picamera.PiCamera()
+cam.start_preview()
+print "press Enter to shoot"
+raw_input("...")
+cam.hflip=True
+cam.vflip=True
+cam.capture(filename)
