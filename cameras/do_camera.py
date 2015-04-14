30d8156c00a30d29b8318dd87a783c1c68b60256
danmaclean <maclean.daniel@gmail.com>
2015-04-14 15:03:54 +0100

    add illumination

--- cameras/do_camera.py
+++ cameras/do_camera.py
@@ -1,10 +1,10 @@
 
 import picamera
 import os
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(17, GPIO.OUT)
 
 prefix = 'ir_image'
 suffix = '.jpg'
@@ -19,10 +19,10 @@ while filename in files:
 
 cam = picamera.PiCamera()
 cam.start_preview()
-GPIO.output(17,True)
+#GPIO.output(17,True)
 print "press Enter to shoot"
 raw_input("...")
 cam.hflip=True
 cam.vflip=True
 cam.capture(filename)
-GPIO.output(17,False)
+#GPIO.output(17,False)
