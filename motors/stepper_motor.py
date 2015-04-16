293f40d2adc01fd354b3cdb8acb0db42f52fec52
danmaclean <maclean.daniel@gmail.com>
2015-04-16 20:29:38 +0100

    stepper

--- motors/stepper_motor.py
+++ motors/stepper_motor.py
@@ -43,7 +43,7 @@ class StepperMotor:
         [False,False,True,True],
         [False,False,False,True],
         [True,False,False,True]]
-        self.reverse_sequence = self.sequence.reverse()
+        self.reverse_sequence = list(reversed(self.sequence.reverse()))
         print self.reverse_sequence
     
     def clockwise(self,steps=512,delay=(1/1000.0)):
