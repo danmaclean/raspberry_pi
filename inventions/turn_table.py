670a4d2ca89efea0ba682c18a381cd6a1af31446
danmaclean <maclean.daniel@gmail.com>
2015-04-17 10:32:05 +0100

    turntable

--- inventions/turn_table.py
+++ inventions/turn_table.py
@@ -32,7 +32,7 @@ class TurnTable:
     
     def get_steps_to_sections(self,sections):
         '''gets the number of steps to each section from the origin'''
-        distances = [(self.steps_between_sections * i) - self.steps_between_sections for i in range(1,sections)]
+        distances = [(self.steps_between_sections * i) - self.steps_between_sections for i in range(1,sections + 1)]
         print "distances", distances
         distance_to_section = {}
         for i in range(1, sections):
