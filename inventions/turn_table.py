e5361370bc2ddffdbcae2cf8ae1119ccf34b0d6e
danmaclean <maclean.daniel@gmail.com>
2015-04-17 09:48:13 +0100

    turntable

---
+++ inventions/turn_table.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+turn_table.py
+
+
+Created by Dan MacLean (TSL) on 2015-04-16.
+Copyright (c) 2015 Dan MacLean. All rights reserved.
+"""
+import StepperMotor
+
+class TurnTable:
+    
+    def __init__(self, sections=6, motor_pins=[17,18,21,22]):
+        self.sections = sections
+        self.motor = StepperMotor(motor_pins)
+        self.steps_between_sections = TurnTable.get_steps_between_sections(sections)
+        self.steps_to_sections_from_origin = TurnTable.get_steps_to_sections(sections)
+        self.current_section = 1
+    
+    def get_steps_between_sections(self,sections):
+        '''divides the 512 steps into specified sections'''
+        return 512.0 / sections
+    
+    def get_steps_to_sections(self,sections):
+        '''gets the number of steps to each section from the origin'''
+        distances = [(self.steps_between_sections * i) - self.steps_between_sections for i in range(1,sections + 1)]
+        distance_to_section = {}
+        for i in range(1, sections + 1):
+            distance_to_section[i] = distances[i]
+        return distance_to_section 
+    
+    def go_to_section(self,target_section):
+        '''sends the motor to a particular section'''
+        steps_to_go = TurnTable.get_steps_to(self,target_section)
+        if steps_to_go < 0:
+            self.motor.anticlockwise(abs(steps_to_go))
+        else:
+            self.motor.clockwise(steps_to_go)
+    
+    def get_steps_to(self,target_section):
+        '''gets the number of steps from self.current_section to target section.'''
+        return self.steps_to_section[target_section] - current
+        
+
+try:
+    table = TurnTable.new(6,[17,18,21,22])
+    stops = [1,5,4,2,3]
+    for s in stops:
+        table.go_to_section(s)
+except:
+    "huh?"
--- motors/stepper_motor.py
+++ motors/stepper_motor.py
@@ -22,11 +22,11 @@ class StepperMotor:
     28BJY-48 with ULN2003 control board
     '''
     
-    def __init__(self, pin1,pin2,pin3,pin4):
-        self.pin1 = pin1
-        self.pin2 = pin2
-        self.pin3 = pin3
-        self.pin4 = pin4
+    def __init__(self, pins = [17,18,21,22] ):
+        self.pin1 = pins[0]
+        self.pin2 = pins[1]
+        self.pin3 = pins[2]
+        self.pin4 = pins[3]
         GPIO.setup(self.pin1, GPIO.OUT)
         GPIO.setup(self.pin2, GPIO.OUT)
         GPIO.setup(self.pin3, GPIO.OUT)
@@ -73,64 +73,10 @@ class StepperMotor:
 
 try:
     print "trying motor"
-    motor = StepperMotor(17,18,21,22)
+    motor = StepperMotor([17,18,21,22])
     motor.clockwise(512)
     motor.anticlockwise(512)
     GPIO.cleanup()
 except:
     GPIO.cleanup()
 
-'''
-def main():
-	GPIO.setmode(GPIO.BCM)
-	enable_pin = 18
-	coil_A_1_pin = 4
-	coil_A_2_pin = 17
-	coil_B_1_pin = 23
-	coil_B_2_pin = 24
-	
-	GPIO.setup(enable_pin, GPIO.OUT)
-	GPIO.setup(coil_A_1_pin,GPIO.OUT)
-	GPIO.setup(coil_A_2_pin, GPIO.OUT)
-	GPIO.setup(coil_B_1_pin, GPIO.OUT)
-	GPIO.setup(coil_B_2_pin, GPIO.OUT)
-	GPIO.output(enable_pin,1)
-	
-	def forward(delay,steps):
-		for i in range(0,steps):
-			setStep(1,0,1,0)
-			time.sleep(delay)
-			setStep(0,1,1,0)
-			time.sleep(delay)
-			setStep(0,1,0,1)
-			time.sleep(delay)
-			setStep(1,0,0,1)
-			time.sleep(delay)
-	
-	def backwards(delay,steps):
-		for i in range(0,steps):
-			setStep(1,0,0,1)
-			time.sleep(delay)
-			setStep(0,1,0,1)
-			time.sleep(delay)
-			setStep(0,1,1,0)
-			time.sleep(delay)
-			setStep(1,0,1,0)
-			time.sleep(delay)
-	
-	def setStep(w1,w2,w3,w4):
-		GPIO.output(coil_A_1_pin, w1)
-		GPIO.output(coil_A_2_pin, w2)
-		GPIO.output(coil_B_1_pin, w3)
-		GPIO.output(coil_B_2_pin, w4)
-	
-	while True:
-		delay = raw_input("Delay between steps (ms)?")
-		steps = raw_input("How many steps forward?")
-		forward(int(delay) / 1000.0, int(steps) )
-		steps = raw_input("How many steps backwawrds?")
-		backwards(int(delay) / 1000.0, int(steps) )
-
-if __name__ == '__main__':
-	main()
-'''
