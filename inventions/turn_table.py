c6492fd8f7b43533770a38143d5a9024081e4584
danmaclean <maclean.daniel@gmail.com>
2015-04-17 11:03:16 +0100

    turntable

--- inventions/turn_table.py
+++ inventions/turn_table.py
@@ -27,12 +27,30 @@ class TurnTable:
         print "current section", self.current_section
     
     def get_steps_between_sections(self,sections):
-        '''divides the 512 steps into specified sections'''
-        return 512.0 / sections
+        '''divides the 512 steps into specified sections.
+        gives an integer on purpose - we must use an integer value of 
+        steps'''
+        return 512 / sections
     
     def get_steps_to_sections(self,sections):
-        '''gets the number of steps to each section from the origin'''
+        '''gets the number of steps to each section from the origin.
+        Since the number of steps to each section might be a decimal,
+        the extra steps are saved and shared whole between each section.
+        
+        EG. 512.0 / 6 = 85.3 
+        but as we must work in integers..
+        512 / 6 = 85
+        85 * 6 = 510
+        therefore we have 2 steps left over. 
+        Put the first extra step in section 1, then the second in section 2 etc
+        '''
         distances = [(self.steps_between_sections * i) - self.steps_between_sections for i in range(1,sections + 1)]
+        ## now we need to add in the missing steps if any ( we should always have less remaining than there are sections)
+        missing_steps = (512 - ((512/sections) * sections) )
+        if missing_steps > 0:
+            for m in range(1,missing_steps):
+                distances[m] += 1
+        
         print "distances", distances
         distance_to_section = {}
         for i in range(0, sections):
